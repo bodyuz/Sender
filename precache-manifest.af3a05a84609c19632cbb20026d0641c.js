5ea527726a3fd2d9a64e5ab740f9f73c31a5329a
bodyuz <54287040+bodyuz@users.noreply.github.com>
2019-08-27 13:18:44 +0300

    Add files via upload

---
+++ precache-manifest.af3a05a84609c19632cbb20026d0641c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ff7fcaea4e76e9f3a42a7351ba92a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64d29108945749de8290",
+    "url": "/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "64b93f60272130ca22be",
+    "url": "/static/css/main.93d0e5f7.chunk.css"
+  },
+  {
+    "revision": "64d29108945749de8290",
+    "url": "/static/js/2.7664ad1d.chunk.js"
+  },
+  {
+    "revision": "64b93f60272130ca22be",
+    "url": "/static/js/main.6e87e379.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
